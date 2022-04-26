9ad2f648a1f3761d0d0b425ef0d73c911af61de1
mbassett <mbassett@AP-mbassett-W1.adprod.bmc.com>
2022-04-26 13:50:08 +0800

    Demo BMC Software (Hong Kong) Ltd.

--- GenAppCore/Sources/SalesRepManagement/Payroll/Batch/CWXTSUBC.cbl
+++ GenAppCore/Sources/SalesRepManagement/Payroll/Batch/CWXTSUBC.cbl
@@ -10,7 +10,7 @@
       *  CALCULATES COMMISIONS FOR SALES AND MANAGEMENT EMPLOYEE       *
       *  BASED ON MONTHLY SALES RATES                                  *
       *                                                                *
-      *  Demo 26-04-2022 1:15 PM                                      *
+      *  Demo 26-04-2022 1:45 PM                                      *
       ******************************************************************
        ENVIRONMENT DIVISION.
        DATA DIVISION.
